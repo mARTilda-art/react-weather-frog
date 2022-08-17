4b1fd0330afaae27df12ae04967f866d14490246
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-17 20:17:12 +0200

    initial publish repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Berlin" />
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://www.martilda.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mARTilda
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/mARTilda-art/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and is hosted on{" "}
+          <a
+            href="https://symphonious-pegasus-adb3f8.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
