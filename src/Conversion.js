cb858a98a97aa2b41a027ea3bc98a192eabf01fa
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-20 22:46:35 +0200

    added conversion

--- src/Conversion.js
+++ src/Conversion.js
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Conversion.css";
 
-export default function Conversion() {
+export default function Conversion(props) {
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  function handleDegreeChange(event) {
+    let fahrenheit = (event.target.value * 9) / 5 + 32;
+    setFahrenheit(fahrenheit);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div class="col-12">
-      <table class="conversionalert table">
-        <thead>
-          <tr>
-            <th scope="col" colspan="7">
-              What the <i class="fa-solid fa-frog"></i> are
-            </th>
-            <th scope="col" colspan="1">
-              <input
-                type="text"
-                placeholder="degrees"
-                class="form-control"
-                id="degree-input"
-                autocomplete="off"
-              />
-            </th>
-            <th scope="col" colspan="4">
-              °C in °F?
-            </th>
-            <th scope="col" colspan="2">
-              <div class="col-2">
-                <input
-                  type="submit"
-                  value="Go"
-                  class="btn btn-primary small"
-                  id="buttonConvert"
-                />
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
+    <div className="conversion">
+      <form onSubmit={handleSubmit}>
+        <div className="col-12">
+          <table className="conversionalert table">
+            <thead>
+              <tr>
+                <th scope="col" colSpan="7">
+                  What the <i className="fa-solid fa-frog"></i> are
+                </th>
+                <th scope="col" colSpan="1">
+                  <input
+                    type="text"
+                    placeholder="degrees"
+                    className="form-control"
+                    id="degree-input"
+                    autoComplete="off"
+                    value={props.fahrenheit}
+                    onChange={handleDegreeChange}
+                  />
+                </th>
+                <th scope="col" colSpan="4">
+                  °C in °F?
+                </th>
+                <th scope="col" colSpan="2">
+                  <div className="col-2 fahrenheit">
+                    {Math.round(fahrenheit)} °F
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </form>
     </div>
   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -29,6 +29,8 @@ export default function WeatherInfo(props) {
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
+            <li>Pressure: {props.data.pressure} mb</li>
+            <li>Sunrise: {props.data.sunrise}</li>
           </ul>
         </div>
       </div>
